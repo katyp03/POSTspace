14780767785d624f59796ae401e7b3836c869c6c
Katy Pola <katyp03@gmail.com>
2017-07-17 15:59:40 -0400

    adding timestamps

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, presence: true, length: { in: 1..27 }
+  validates :body, presence: true, length: { minumum: 2, maximum: 250 }
+  validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 	has_many :posts
 	has_many :comments
+	validates :email, presence: true, uniqueness: true, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :fname, presence: true, length: { in: 2..18 }
+	validates :lname, presence: true, length: { in: 2..18 }
 	def full_name
 		"#{fname} #{lname}".strip
 	end
