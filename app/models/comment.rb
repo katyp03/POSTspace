dd4e42c611ee5a9ac87bdb0511c51cfe5030f762
Katy Pola <katyp03@gmail.com>
2017-09-07 23:39:13 -0400

    fixing comment functionality

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :comment, presence: true, length: { in: 2..250 }
+  # validates :body, presence: true, length: { in: 2..250 }
 end
